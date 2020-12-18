6f9176f686c597486ea6062674f9b57ea06398bc
oxy <ashokgaire1@gmail.com>
2020-12-17 23:25:38 -0500

    binary search

--- Agorithms/linear_search.py
+++ Agorithms/linear_search.py
@@ -3,6 +3,8 @@
 Start from the leftmost element of arr[] and one by one compare x with each element of arr[]
 If x matches with an element, return the index.
 If x doesn’t match with any of elements, return -1
+
+time compexity o(n)
 '''
 
 def search(arr, n, x):
